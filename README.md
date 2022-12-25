82abbadcb67c1453a8d63112536aed4e27933ebb
Günter Fischer <fischer-guenter@gmx.net>
2022-12-25 22:27:58 +0100

    Initial setup with bash, git and nvim config

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# docker
+# Docker Images based on Ubuntu 22.04
+
+## Docker Image 'base'
+
+This acts as base container with all basic tools, settings, etc.
+
+It can be built with following command:
+
+```bash
+docker build --network=host -t base ./base
+```
+
+#### bash
+
+This directory contains custom adaptions for bash.
+
+#### git
+
+This directory is intended for the .gitconfig as well as for the ssh keys.
+
+For obvious reasons, I haven't committed my config and keys to this repository.
+This has to be done by everyone himself.
+
+The resulting directory structure should look like this:
+
+```bash
+./base/git/
+├── .gitconfig
+└── .ssh
+    ├── bitbucket.key
+    ├── bitbucket.key.pub
+    ├── github.key
+    └── github.key.pub
+```
+
+If set-up correctly, the ssh keys can be initialized with `ssh_init` from within the container.
+This function is defined in the `bashrc` file.
+
+If no keys are provided, the docker build will fail.
+In this case, the related lines 190-194 has to be commented out in the Dockerfile.
+
+#### nvim
+
+This directory contains the configuration for neovim.
---
+++ base/nvim/after/plugin/lsp.lua
@@ -0,0 +1,67 @@
+-- ~/.config/nvim/after/plugin/lsp.lua
+
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'bashls',
+  'clangd',
+  'cmake',
+  'eslint',
+  'html',
+  'pylsp',
+  'jsonls',
+  'sumneko_lua',
+  'tsserver',
+  'ruby_ls',
+  'yamlls'
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<leader>lp'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<leader>ln'] = cmp.mapping.select_next_item(cmp_select),
+  ['<leader>ly'] = cmp.mapping.confirm({ select = true }),
+  ["<leader>lc"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "<leader>go", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>sym", vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set("n", "<leader>lo", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "<leader>ln", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<leader>lp", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "<leader>fix", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>ref", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>ren", vim.lsp.buf.rename, opts)
+end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = true,
+})
